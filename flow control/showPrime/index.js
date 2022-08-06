108b2f0f37e54cb9e224344228c63db422913fe4
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-08-06 19:25:37 +0200

    second commit

---
+++ flow control/showPrime/index.js
@@ -0,0 +1,10 @@
+showPrimes(20);
+
+function showPrimes(limit) {
+	for (let number = 2; number <= limit; number++) if (isPrime(number)) console.log(number);
+}
+
+function isPrime(number) {
+	for (factor = 2; factor < number; factor++) if (number % factor === 0) return false;
+	return true;
+}
